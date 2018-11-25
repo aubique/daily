5d5e66a864990e83a3f1d91d4e9f19e0cc4395f5
ubikwita <ubikwita@gmail.com>
2018-11-25 23:47:40 +0100

    Mise à jour: 25 Novembre 2018

--- p3_181125_2224.py
+++ p3_181125_2224.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#p3_181125_2224.py
+#p3_181125_2224
 # Tkinter. Lesson 14.
 # Widget Menu
 
@@ -11,17 +11,40 @@ class OpenSaveFile:
     def __init__(self, master):
         self.master = master
         self.create_interface()
+        self.create_menu()
         self.pack_interface()
     def create_interface(self):
         self.text = tk.Text(width=50, height=25)
         self.b1 = tk.Button(text='Open', command=self.insert_text)
         self.b2 = tk.Button(text='Save', command=self.extract_text)
         self.b3 = tk.Button(text='Clear', command=self.clear_text)
+    def create_menu(self):
+        self.mainmenu = tk.Menu(self.master)
+        self.master.config(menu=self.mainmenu)
+        self.filemenu = tk.Menu(self.mainmenu, tearoff=0)
+        self.filemenu.add_command(label='Open...', command=self.insert_text)
+        self.filemenu.add_command(label='Save...', command=self.extract_text)
+        self.filemenu.add_command(label='Clear', command=self.clear_text)
+        self.filemenu.add_command(label='Exit', command=self.exit)
+        self.helpmenu = tk.Menu(self.mainmenu, tearoff=0)
+        self.helpmenu.add_command(label="About", command=self.about)
+        self.mainmenu.add_cascade(label='File', menu=self.filemenu)
+        self.mainmenu.add_cascade(label='Help', menu=self.helpmenu)
+        self.contextmenu = tk.Menu(tearoff=0)
+        self.contextmenu.add_command(label='Clear', command=self.clear_text)
     def pack_interface(self):
         self.text.grid(columnspan=3)
         self.b1.grid(row=1)
         self.b2.grid(row=1, column=1)
         self.b3.grid(row=1, column=2)
+        self.text.bind('<Button-3>', self.popup)
+    def popup(self, event):
+        x, y = event.x, event.y
+        self.contextmenu.post(event.x_root, event.y_root)
+    def about(self):
+        mb.showinfo("Git repository", "https://github.com/\nubqwita/python3_tasks.git")
+    def exit(self):
+        self.master.destroy()
     def insert_text(self):
         try:
             file_name = fd.askopenfilename()
