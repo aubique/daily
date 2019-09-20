0a8ddc564d8e227f4687c1c8fdae66b853295e8c
ubicquita <ubicquita@gmail.com>
2019-09-20 23:00:42 +0100

    Commit java4python Data Types exercises

--- java/src/jse_191119_1348.java
+++ java/src/jse_191119_1348.java
@@ -1,39 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class jse_191119_1348 {
 
     public static void main(String[] args) {
-//        showCarBoolean();
 //        showRemainder();
-        showTemperature();
-    }
-
-    private static void showTemperature() {
-        Double fahr;
-        Double cel;
-        Scanner in;
-
-        in = new Scanner(System.in);
-        System.out.println("Enter the temperature in F: ");
-        fahr = in.nextDouble();
-
-        cel = (fahr - 32) * 5.0 / 9.0;
-        System.out.println("The temperature in C is: " + cel);
-
-        System.exit(0);
-    }
-
-    private static void showCarBoolean() {
-        boolean isCar = false;
-        if (isCar) {
-            System.out.println("isCar isn't true");
-        }
-
-        isCar = true;
-        boolean wasCar = isCar ? true : false;
-        if (wasCar) {
-            System.out.println("wasCar is true");
-        }
+//        useList("tmp/test.dat");
+        useDictionary("tmp/alice30.txt");
     }
 
     private static void showRemainder() {
@@ -45,6 +22,66 @@ public class jse_191119_1348 {
         System.out.println("Remainder doesn't exist: " + isNoRemainder);
         if (!isNoRemainder) {
             System.out.println("Got some remainder");
+        }
+    }
+
+    private static void useList(String filename) {
+        ArrayList<Integer> countList;
+        Scanner data = null;
+        int idx;
+
+        try {
+            data = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            System.out.println("Not found");
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        countList = new ArrayList<Integer>(10);
+        for (int i = 0; i < 10; i++) {
+            countList.add(i, 0);
+        }
+
+        while (data.hasNextInt()) {
+            idx = data.nextInt();
+            countList.set(idx, countList.get(idx) + 1);
+        }
+
+        idx = 0;
+        for (int i = 0; i < countList.size(); i++, idx++) {
+            System.out.println(i + " occured " + countList.get(i) + " times.");
+        }
+    }
+
+    private static void useDictionary(String filename) {
+        Scanner data = null;
+        TreeMap<String, Integer> count;
+        String key, word;
+        Integer value, wordCount;
+
+        try {
+            data = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            System.out.println(filename + "is not found");
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        count = new TreeMap<String, Integer>();
+        while (data.hasNext()) {
+            word = data.next().toLowerCase();
+            wordCount = count.get(word);
+            if (wordCount == null) {
+                wordCount = 0;
+            }
+            count.put(word, ++wordCount);
+        }
+
+        for (Iterator<String> iter = count.keySet().iterator(); iter.hasNext(); ) {
+            key = iter.next();
+            value = count.get(key);
+            System.out.printf("%-20s occured %5d times%n", key, value);
         }
     }
 }
